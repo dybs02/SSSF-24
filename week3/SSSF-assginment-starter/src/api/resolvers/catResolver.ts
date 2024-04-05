2f83cf711873f19a990f10529a88f2c0fa0e06c2
Mateusz <mateuszdybala2@gmail.com>
2024-04-05 17:18:23 +0300

    add TODOs

--- week3/SSSF-assginment-starter/src/api/resolvers/catResolver.ts
+++ week3/SSSF-assginment-starter/src/api/resolvers/catResolver.ts
@@ -1,10 +1,112 @@
-// TODO: Add resolvers for cat
-// 1. Queries
-// 1.1. cats
-// 1.2. catById
-// 1.3. catsByOwner
-// 1.4. catsByArea
-// 2. Mutations
-// 2.1. createCat
-// 2.2. updateCat
-// 2.3. deleteCat
+import { Cat } from "../../interfaces/Cat";
+import catModel from "../models/catModel";
+
+type Coordinates = {
+  lat: Number
+  lng: Number
+}
+
+export default {
+  Query: {
+    cats: async (): Promise<Cat[]> => {
+      return await catModel.find();
+    },
+    catById: async (
+      _parent: undefined,
+      args: { id: string },
+    ): Promise<Cat> => {
+      const cat = await catModel.findById(args.id).populate('owner');
+      if (!cat) {
+        throw new Error('Cat not found');
+      }
+      return cat;
+    },
+    catsByOwner: async (
+      _parent: undefined,
+      args: { ownerId: String },
+    ): Promise<Cat[]> => {
+      const cats = await catModel.find({ owner: args.ownerId }).populate('owner');
+      if (!cats) {
+        throw new Error('Cat not found');
+      }
+      return cats;
+    },
+    catsByArea: async (
+      _parent: undefined,
+      args: {
+        topRight: Coordinates,
+        bottomLeft: Coordinates
+      },
+    ): Promise<Cat[]> => {
+      const cats = await catModel.find({
+        location: {
+          $geoWithin: {
+            $geometry: {
+              type: 'Polygon',
+              coordinates: [[
+                [args.topRight.lat, args.topRight.lng],
+                [args.topRight.lat, args.bottomLeft.lng],
+                [args.bottomLeft.lat, args.bottomLeft.lng],
+                [args.bottomLeft.lat, args.topRight.lng],
+                [args.topRight.lat, args.topRight.lng],
+              ]]
+            }
+          }
+        }
+      }).populate('owner');
+
+      if (!cats) {
+        throw new Error('Cat not found');
+      }
+      return cats;
+    },
+  },
+  Mutation: {
+    createCat: async (
+      _parent: undefined,
+      args: {
+        cat_name: String,
+        weight: Number,
+        birthdate: Date,
+        owner: String,
+        location: { type: String, coordinates: [Number] },
+        filename: String,
+      },
+    ): Promise<Cat> => {
+      const cat = await catModel.create(args);
+      if (!cat) {
+        throw new Error('Cat not found');
+      }
+      return cat;
+    },
+    updateCat: async (
+      _parent: undefined,
+      args: {
+        id: String,
+        cat_name: String,
+        weight: Number,
+        birthdate: Date
+      },
+    ): Promise<Cat> => {
+      const cat = await catModel.findByIdAndUpdate(
+        args.id,
+        args,
+        { new: true },
+      );
+      if (!cat) {
+        throw new Error('Cat not found');
+      }
+      return cat;
+    },
+    deleteCat: async (
+      _parent: undefined,
+      args: { id: string },
+    ): Promise<Cat> => {
+      const cat = await catModel.findByIdAndDelete(args.id);
+      if (!cat) {
+        throw new Error('Cat not found');
+      }
+      return cat;
+    }
+  },
+};
--- week3/SSSF-assginment-starter/src/api/resolvers/userResolver.ts
+++ week3/SSSF-assginment-starter/src/api/resolvers/userResolver.ts
@@ -1,8 +1,60 @@
-// TODO: Add resolvers for user
-// 1. Queries
-// 1.1. users
-// 1.2. userById
-// 2. Mutations
-// 2.1. createUser
-// 2.2. updateUser
-// 2.3. deleteUser
+import { User } from "../../interfaces/User";
+import userModel from "../models/userModel";
+
+
+export default {
+  Query: {
+    users: async (): Promise<User[]> => {
+      return await userModel.find();
+    },
+    userById: async (
+      _parent: undefined,
+      args: { id: string },
+    ): Promise<User> => {
+      const user = await userModel.findById(args.id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    },
+  },
+  Mutation: {
+    createUser: async (
+      _parent: undefined,
+      args: { user_name: String, email: String },
+    ): Promise<User> => {
+      const user = await userModel.create({
+        user_name: args.user_name,
+        email: args.email
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    },
+    updateUser: async (
+      _parent: undefined,
+      args: { user_name: String, id: String },
+    ): Promise<User> => {
+      const user = await userModel.findByIdAndUpdate(
+        args.id,
+        args,
+        { new: true },
+      );
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    },
+    deleteUser: async (
+      _parent: undefined,
+      args: { id: string },
+    ): Promise<User> => {
+      const user = await userModel.findByIdAndDelete(args.id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    },
+  },
+}; 
